c6b881c69443d1f450f9fa4a6b9e035b3968f748
Leonid Malyshev <lokiwish@list.ru>
2024-04-22 22:49:16 +0300

    add chat

---
+++ CommonChat/DTO/ChatMessage.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+
+namespace CommonChat.DTO
+{
+    public class ChatMessage
+    {
+        public int? Id { get; set; }
+        public string FromName { get; set; }
+        public string ToName { get; set; }
+        public string Text { get; set; }
+
+
+        public string ToJson() => JsonSerializer.Serialize(this);
+        public static ChatMessage FromJson(string text) => JsonSerializer.Deserialize<ChatMessage>(text);
+    }
+
+    public enum Command
+    {
+        Message,
+        Confirmation,
+        Register
+    }
+}
